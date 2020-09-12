5f305321ff13dc855dad7ca4b9b35a40984c86b0
kamurod <kamiluroda@gmail.com>
2020-09-12 19:19:22 +0200

    update app2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
                   Edit <code>src/App.js</code> and save to reload.
-                  feature branch test
+                  feature branch test test
         </p>
         <a
           className="App-link"
